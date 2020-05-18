2e9c4244f8d860e8992063ed3dc26709fce96752
Raju.Motapothula <Raju.Motapothula@IN01N02625.icepor.com>
2020-05-18 11:19:55 +0530

    intial commit for project

---
+++ gitjavademo/src/com/raju/test/Test.java
@@ -0,0 +1,13 @@
+package com.raju.test;
+
+import com.raju.util.CommonUtil;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Test "+CommonUtil.getDefaultDate());
+	}
+
+}
---
+++ gitjavademo/src/com/raju/util/CommonUtil.java
@@ -0,0 +1,11 @@
+package com.raju.util;
+
+import java.util.Date;
+
+public class CommonUtil {
+
+	public static Date getDefaultDate()
+	{
+		return new Date();
+	}
+}
