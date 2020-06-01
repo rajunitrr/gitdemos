7dc1e9548419341eeb5c7776d17f35b39f2735b7
Raju.Motapothula <Raju.Motapothula@IN01N02625.icepor.com>
2020-06-01 16:27:21 +0530

    changing port num

--- mycoolapp/src/main/java/com/raju/springboot/demo/rest/TestController.java
+++ mycoolapp/src/main/java/com/raju/springboot/demo/rest/TestController.java
@@ -1,16 +1,18 @@
-package com.raju.springboot.controller;
+package com.raju.springboot.demo.rest;
 
-import java.time.LocalDate;
+
+import java.util.Date;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+	
 	@GetMapping("/")
 	public String sayHello() {
 		
-		return "Hello man!"+LocalDate.now();
+		return "Hello man!"+new Date();
 	}
 
 }
